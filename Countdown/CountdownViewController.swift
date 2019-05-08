d41a84570a16f4f0df97d83d9c881b694e310386
Nelson Gonzalez <nelglezfl@gmail.com>
2019-05-08 13:37:41 -0400

    Update start button to start the countdown, conformed to delegate protocol

--- Countdown/CountdownViewController.swift
+++ Countdown/CountdownViewController.swift
@@ -15,18 +15,59 @@ class CountdownViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     
     
+    let countdown = Countdown()
+    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss.SS"
+        formatter.timeZone = TimeZone.init(secondsFromGMT: 0)
+        return formatter
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        countdown.duration = 5 //seconds
+        countdown.delegate = self
+        
+        timeLabel.font = .monospacedDigitSystemFont(ofSize: timeLabel.font.pointSize, weight: .regular)
+
+    }
+    
+    private func updateViews() {
+//        timeLabel.text = "\(countdown.timeRemaining)"
+         timeLabel.text = string(from: countdown.timeRemaining)
+        
     }
 
+    //5.019769 ... => 00:00:05.02
+    func string(from duration: TimeInterval) -> String {
+        let date = Date(timeIntervalSinceReferenceDate: duration)
+        return dateFormatter.string(from: date)
+    }
+    
+   
 
     @IBAction func startButtonPressed(_ sender: UIButton) {
+        countdown.start()
     }
     
     @IBAction func resetButtonPressed(_ sender: UIButton) {
+        
+        countdown.reset()
     }
+    
+}
+
+extension CountdownViewController: CountdownDelegate {
+    func countdownDidUpdate(timeRemaining: TimeInterval) {
+       updateViews()
+    }
+    
+    func countdownDidFinish() {
+      
+    }
+    
     
 }
 
